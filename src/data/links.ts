9a3b22cb7c6dd74e009d6badab36d54c3f691010
kananigara <kananigara@gmail.com>
2025-01-12 15:56:59 +0700

    Pertama

---
+++ src/data/links.ts
@@ -0,0 +1,19 @@
+import TwitterIcon from '@/components/icons/TwitterIcon'
+import GithubIcon from '@/components/icons/GithubIcon'
+//import GithubIcon from '@/components/icons/GithubIcon'
+
+
+// ADD YOUR SOCIAL NETWORKS HERE
+export const SOCIALNETWORKS = [
+	{
+		name: 'Github',
+		url: 'https://github.com/platformdigitalgw',
+		icon: GithubIcon
+	},
+
+	{
+		name: 'Twitter',
+		url: '#',
+		icon: TwitterIcon
+	}
+] as const
